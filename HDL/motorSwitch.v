20d62ae643c8e44a7950c21b2d99f77d419d1398
atangzwj <atangzwj@outlook.com>
2017-07-05 10:23:57 -0700

    Added constraints file, edited motorSwitch.v to match constraints

--- HDL/motorSwitch.v
+++ HDL/motorSwitch.v
@@ -20,12 +20,12 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 // SW[0] turns on the motors and drives the bot forward at full speed
-module motorSwitch (JB, SW);
-   output [7:0] JB;
-   input  [3:0] SW;
+module motorSwitch (jb, sw);
+   output [7:0] jb;
+   input  [3:0] sw;
 
-   assign JB[1] = 1'b1; // DIR1
-   assign JB[5] = 1'b0; // DIR2
-   assign JB[0] = SW[0]; // EN1
-   assign JB[4] = SW[0]; // EN2
+   assign jb[1] = 1'b1; // DIR1
+   assign jb[5] = 1'b0; // DIR2
+   assign jb[0] = sw[0]; // EN1
+   assign jb[4] = sw[0]; // EN2
 endmodule
