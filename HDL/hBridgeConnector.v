9c07d29cd04c8f1808772cc15e90d3028153a9ee
atangzwj <atangzwj@outlook.com>
2017-07-11 09:15:56 -0700

    hBridgeConnector.v can take inputs now

--- HDL/hBridgeConnector.v
+++ HDL/hBridgeConnector.v
@@ -33,14 +33,28 @@ module hBridgeConnector(
    output [3:0] gpio2_io_i,
    input  [3:0] gpio2_io_o,
    input  [3:0] gpio2_io_t,
-   input  [1:0] pwm
+   input  [1:0] pwm,
+   output       m1_feedback,
+   output       m2_feedback
    );
+
+   /* Top row */
+   assign in_top_bus_T = 4'b1100; // Disable/enable buffers
    
    assign in_top_bus_O[0] = pwm[0];                      // EN1
-   assign in_top_bus_O[1] = gpio_io_o[0] | gpio_io_o[1]; // DIR1
-   assign gpio_io_i[3:2] = in_top_bus_O[3:2];            // S1B and S1A
+   assign in_top_bus_O[1] = gpio_io_o[0];// | gpio_io_o[1]; // DIR1
+   assign m1_feedback     = in_top_bus_I[2];             // S1A
+   
+   /* Bottom row */
+   assign in_bottom_bus_T = 4'b1100; // Disable/enable buffers
    
    assign in_bottom_bus_O[0] = pwm[1];                        // EN2
-   assign in_bottom_bus_O[1] = gpio2_io_o[0] | gpio2_io_o[1]; // DIR2
-   assign gpio2_io_i[3:2] = in_bottom_bus_O[3:2];             // S2B and S2A
+   assign in_bottom_bus_O[1] = gpio2_io_o[0];// | gpio2_io_o[1]; // DIR2
+   assign m2_feedback        = in_bottom_bus_I[2];            // S2A
+   
+   /* Unused ports */
+   assign in_top_bus_O[3:2]    = 2'b00;
+   assign in_bottom_bus_O[3:2] = 2'b00;
+   assign gpio_io_i            = 4'b0000;
+   assign gpio2_io_i           = 4'b0000;
 endmodule
