2af193639abe686cb8741550536f0b323e606faa
atangzwj <atangzwj@outlook.com>
2017-07-11 09:16:36 -0700

    Began work on milestone 3

--- Software/artyBot.c
+++ Software/artyBot.c
@@ -8,6 +8,7 @@
  */
 
 #include <stdlib.h>
+#include "xil_io.h"
 #include "artyBot.h"
 
 void initIO() {
@@ -22,7 +23,68 @@ void initIO() {
    xgpio_cfg1 = XGpio_LookupConfig(GPIO_1_DEV_ID);
 
    // Set directions
-   XGpio_SetDataDirection(xgpio0, MOTOR1_CHANNEL, 0xC);
-   XGpio_SetDataDirection(xgpio0, MOTOR2_CHANNEL, 0xC);
+   XGpio_SetDataDirection(xgpio0, M1_CHANNEL, 0xC);
+   XGpio_SetDataDirection(xgpio0, M2_CHANNEL, 0xC);
    XGpio_SetDataDirection(xgpio1, SW_CHANNEL, 0xF);
 }
+
+// Turns both motors on at half speed when SW0 is toggled on (milestone 2)
+void motorSwitch() {
+   PWM_Set_Period(PWM_BASEADDR, PWM_PER);
+   PWM_Set_Duty(PWM_BASEADDR, PWM_DUTY, PWM_M1);
+   PWM_Set_Duty(PWM_BASEADDR, PWM_DUTY, PWM_M2);
+
+   PWM_Disable(PWM_BASEADDR); // Disable PWM before changing motor directions
+
+   MOTOR1_FORWARD; // Set motor directions to forward
+   MOTOR2_FORWARD;
+
+   int sw0 = XGpio_DiscreteRead(xgpio1, SW_CHANNEL) & 0x1;
+
+   while (1) {
+      if (sw0) {
+         PWM_Enable(PWM_BASEADDR);
+      } else {
+         PWM_Disable(PWM_BASEADDR);
+      }
+      sw0 = XGpio_DiscreteRead(xgpio1, SW_CHANNEL) & 0x1;
+   }
+}
+
+void measureSpeed() {
+   print("Measuring speed...\n\r");
+   clearCounts();
+   while (1) {
+      u32 count = Xil_In32(EDGE_COUNTER_0_BASEADDR + SENS_COUNT_OFFSET);
+      if (count >= 0x00000FFF) {
+         clearCounts();
+      }
+      xil_printf("%3x\r", count);
+   }
+}
+
+void clearCounts() {
+   Xil_Out8(EDGE_COUNTER_0_BASEADDR, 0x1);
+   Xil_Out8(EDGE_COUNTER_0_BASEADDR, 0x0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Software/artyBot.h
+++ Software/artyBot.h
@@ -22,20 +22,25 @@
 #define PWM_BASEADDR XPAR_PWM_0_PWM_AXI_BASEADDR
 #define PWM_PER      0x00029000 // 2ms
 #define PWM_DUTY     0x00014800 // 50% duty cycle
-#define PWM_0        0
-#define PWM_1        1
+#define PWM_M1       0
+#define PWM_M2       1
 
 #define GPIO_0_DEV_ID XPAR_AXI_GPIO_0_DEVICE_ID
 #define GPIO_1_DEV_ID XPAR_AXI_GPIO_1_DEVICE_ID
 
-#define MOTOR1_CHANNEL 1
-#define MOTOR2_CHANNEL 2
-#define SW_CHANNEL     1
+#define M1_CHANNEL 1
+#define M2_CHANNEL 2
+#define SW_CHANNEL 1
 
-#define MOTOR1_FORWARD  XGpio_DiscreteWrite(xgpio0, MOTOR1_CHANNEL, 0x3)
-#define MOTOR1_BACKWARD XGpio_DiscreteWrite(xgpio0, MOTOR1_CHANNEL, 0x0)
-#define MOTOR2_FORWARD  XGpio_DiscreteWrite(xgpio0, MOTOR2_CHANNEL, 0x0)
-#define MOTOR2_BACKWARD XGpio_DiscreteWrite(xgpio0, MOTOR1_CHANNEL, 0x3)
+#define MOTOR1_FORWARD  XGpio_DiscreteWrite(xgpio0, M1_CHANNEL, 0x1) // changed
+#define MOTOR1_BACKWARD XGpio_DiscreteWrite(xgpio0, M1_CHANNEL, 0x0) // 0x3 to
+#define MOTOR2_FORWARD  XGpio_DiscreteWrite(xgpio0, M2_CHANNEL, 0x0) // 0x1
+#define MOTOR2_BACKWARD XGpio_DiscreteWrite(xgpio0, M1_CHANNEL, 0x1)
+
+#define EDGE_COUNTER_0_BASEADDR XPAR_EDGE_COUNTER_0_S00_AXI_BASEADDR
+#define EDGE_COUNTER_1_BASEADDR XPAR_EDGE_COUNTER_1_S00_AXI_BASEADDR
+#define SENS_COUNT_OFFSET 0x04
+#define CLK_COUNT_OFFSET  0x08
 
 
 /************ Global Variables ************/
@@ -49,5 +54,11 @@ XGpio_Config *xgpio_cfg1;
 /************ Function Prototypes ************/
 
 void initIO();
+
+void motorSwitch();
+
+void measureSpeed();
+
+void clearCounts();
 
 #endif /* SRC_ARTYBOT_H_ */
