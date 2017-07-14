58f4b8d1cb84b0d861fe3b3692cbb63793a7a359
atangzwj <atangzwj@outlook.com>
2017-07-14 10:15:55 -0700

    Fixed typo in macro definition and changed motor directions in macro

--- Software/artyBot.h
+++ Software/artyBot.h
@@ -32,10 +32,10 @@
 #define M2_CHANNEL 2
 #define SW_CHANNEL 1
 
-#define MOTOR1_FORWARD  XGpio_DiscreteWrite(xgpio0, M1_CHANNEL, 0x1)
-#define MOTOR1_BACKWARD XGpio_DiscreteWrite(xgpio0, M1_CHANNEL, 0x0)
-#define MOTOR2_FORWARD  XGpio_DiscreteWrite(xgpio0, M2_CHANNEL, 0x0)
-#define MOTOR2_BACKWARD XGpio_Discret29000eWrite(xgpio0, M2_CHANNEL, 0x1)
+#define MOTOR1_FORWARD  XGpio_DiscreteWrite(xgpio0, M1_CHANNEL, 0x0)
+#define MOTOR1_BACKWARD XGpio_DiscreteWrite(xgpio0, M1_CHANNEL, 0x1)
+#define MOTOR2_FORWARD  XGpio_DiscreteWrite(xgpio0, M2_CHANNEL, 0x1)
+#define MOTOR2_BACKWARD XGpio_DiscreteWrite(xgpio0, M2_CHANNEL, 0x0)
 
 #define EDGE_COUNTER_0_BASEADDR XPAR_EDGE_COUNTER_0_S00_AXI_BASEADDR
 #define EDGE_COUNTER_1_BASEADDR XPAR_EDGE_COUNTER_1_S00_AXI_BASEADDR
